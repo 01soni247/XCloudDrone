3dfd489dbec1067150a83221a64a78ef8abf16b1
SeaOcpus <77526226+Kentanglu@users.noreply.github.com>
2022-10-25 16:53:29 +0700

    Update lancelot.sh

--- lancelot.sh
+++ lancelot.sh
@@ -91,7 +91,7 @@ function errorr() {
 function zipping() {
 tg_post_msg "Proses Zipping Kernel R9..."
     cd AnyKernel || exit 1
-    zip -r9 [$VERSION]Lancelot[TESTSOURCE][$KERNELNAME]-$DATE.zip * -x .git README.md *placeholder
+    zip -r9 [$VERSION]Lancelot[HotTEST][$KERNELNAME]-$DATE.zip * -x .git README.md *placeholder
     cd ..
 }
 compile
