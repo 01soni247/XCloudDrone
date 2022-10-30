91bee03dc3aed32be396c5a2ce10628ff7f6b213
SeaOcpus <77526226+Kentanglu@users.noreply.github.com>
2022-10-30 21:48:28 +0700

    tidy up

--- lancelot.sh
+++ lancelot.sh
@@ -4,8 +4,6 @@
 #
 
 # Main
-export LOCALVERSION=💫
-VERSION=XQ1.5z
 MainPath=$(pwd)
 MainClangPath=${MainPath}/toolchains/clang
 MainClangZipPath=${MainPath}/clang-zip
@@ -14,14 +12,26 @@ GCCaPath=${MainPath}toolchains/GCC64
 GCCbPath=${MainPath}toolchains/GCC32
 MainZipGCCaPath=${MainPath}/GCC64-zip
 MainZipGCCbPath=${MainPath}/GCC32-zip
-CLANG_ROOTDIR=$(pwd)/clang
+
+# Clone Compiler
+git clone --depth=1 https://gitlab.com/RyuujiX/atom-x-clang clang
+
+#Main2
+VERSION=XQ1.6
 KERNELNAME=Sea
-export KBUILD_BUILD_USER=Asyanx
-export KBUILD_BUILD_HOST=CircleCi
+KERNEL_ROOTDIR=$(pwd)
+DEVICE_DEFCONFIG=lancelot_defconfig
+DEVICE_CODENAME=Lancelot
+export KERNEL_NAME=$(cat "arch/arm64/configs/$DEVICE_DEFCONFIG" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
+CLANG_ROOTDIR=$(pwd)/clang
+CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
+export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
 IMAGEL=$(pwd)/lancelot/out/arch/arm64/boot/Image.gz-dtb
 DTBOl=$(pwd)/lancelot/out/arch/arm64/boot/dtbo.img
 DTB=$(pwd)/lancelot/out/arch/arm64/boot/dts/mediatek/mt6768.dtb
-DEVICE_CODENAME=Lancelot
+export KBUILD_BUILD_USER=Asyanx
+export KBUILD_BUILD_HOST=CircleCi
 
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
@@ -37,14 +47,17 @@ tg_post_msg() {
 
 }
 
+# Post Main Information
+tg_post_msg "<b>KernelCompiler</b>%0AKernel Name : <code>${KERNEL_NAME}</code>%0AKernel Version : <code>${KERVER}</code>%0ABuild Date : <code>${DATE}</code>%0ABuilder Name : <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host : <code>${KBUILD_BUILD_HOST}</code>%0ADevice Defconfig: <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
+
 # Compile
 compile(){
-git clone --depth=1 https://gitlab.com/RyuujiX/atom-x-clang clang
 git clone --depth=1 https://$githubKey@github.com/Kentanglu/Sea-XQ.git -b main lancelot
 PATH="${PATH}:$(pwd)/clang/bin"
 cd lancelot
+export LOCALVERSION=🦭
 tg_post_msg "<b>XCloudDrone:</b><code>Build R9 DI Mulai</code>"
-make -j$(nproc) O=out ARCH=arm64 lancelot_defconfig
+make -j$(nproc) O=out ARCH=arm64 $DEVICE_DEFCONFIG
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
     LD=${CLANG_ROOTDIR}/bin/ld.lld \
@@ -53,7 +66,8 @@ make -j$(nproc) ARCH=arm64 O=out \
     OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
     OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi- \
+    2>&1 | tee error.log
 
    if ! [ -a "$IMAGEL" ]; then
 	errorr
@@ -78,7 +92,7 @@ tg_post_msg "Mengirim Kernel R9..."
 # Fin Error
 function errorr() {
     cd out
-    LOG=$(echo *.log)
+    LOG=$(echo error.log)
     curl -d document=@"$LOG" \
         -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
