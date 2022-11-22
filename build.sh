d4aec53a2db3a8c64426187b234378cf6413c78b
SeaOcpus <77526226+Kentanglu@users.noreply.github.com>
2022-11-22 23:52:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ KERNELNAME=Sea
 NAME=Azura
 UseZyCLLVM="n"
 UseGCCLLVM="n"
-UseGoldBinutils="y"
+UseGoldBinutils="m"
 UseOBJCOPYBinutils="n"
 
 CloneKernel(){
@@ -77,7 +77,7 @@ export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
 tg_post_msg "<b>KernelCompiler</b>%0AKernel Name : <code>${KERNEL_NAME}</code>%0AKernel Version : <code>${KERVER}</code>%0ABuild Date : <code>${DATE}</code>%0ABuilder Name : <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host : <code>${KBUILD_BUILD_HOST}</code>%0ADevice Defconfig: <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${ClangPath}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
 tg_post_msg "<b>XCloudDrone:</b><code>Compile $DEVICE_CODENAME DI Mulai</code>"
 cd $DEVICE_CODENAME
-export LOCALVERSION=1t/Azura🫧
+export LOCALVERSION=1/Azura🌺
     MorePlusPlus=" "
     PrefixDir=""
     if [[ "$UseZyCLLVM" == "y" ]];then
